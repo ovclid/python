7cec9ff474c7a7c586f08131f8042acb7322792f
ovclid <53159064+ovclid@users.noreply.github.com>
2020-09-21 17:18:24 +0900

    Update memory_check.py

--- memory_check.py
+++ memory_check.py
@@ -4,3 +4,14 @@ from binascii import hexlify
 
 a = 12314
 print(hexlify(string_at(id(a), getsizeof(a))))
+
+r = open("전체무장.txt", "r")
+result = r.read()
+
+file = open('전체무장_메모리모습.txt', 'w')
+file.write(str(hexlify(string_at(id(result), getsizeof(result)))))
+file.close()
+
+for i in range(10):
+	print(ord(result[i]), hex(ord(result[i])), bin(ord(result[i])))
+
